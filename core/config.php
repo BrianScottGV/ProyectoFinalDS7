b5105b7e365ee656ad8527fdc435a316f57738d6
conex0770@gmail.com <conex0770@gmail.com>
2025-06-22 18:49:23 -0500

    config y database

--- core/config.php
+++ core/config.php
@@ -2,7 +2,7 @@
 
 //DEFINICION DE CONSTANTES DE LA WEB
 define('DB_HOST','localhost');
-define('DB_NAME','recomdaciones_db');
+define('DB_NAME','recomendaciones_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('BASE_URL', '');
